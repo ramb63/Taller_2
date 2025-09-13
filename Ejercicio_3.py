97a5dcb7e24f67bce9b13b337a8e99e85cfedb63
Fabian <orleyfabian.r@gmail.com>
2025-09-12 19:00:49 -0500

    Add script to calculate and display the sum of numbers from 1 to 100

---
+++ Ejercicio_3.py
@@ -0,0 +1,5 @@
+#Suma 1..100 Calcula y muestra la suma de los números del 1 al 100 con un acumulador
+suma = 0
+for i in range(1,101):
+    suma = suma + i
+print(f"suma de 1 al 100 da un total de:", suma)
