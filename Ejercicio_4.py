015fabe4ca62b0a7bd552dc88988da0505725cc5
Fabian <orleyfabian.r@gmail.com>
2025-09-12 19:49:21 -0500

    Refactor script to improve clarity and maintainability

--- Ejercicio_4.py
+++ Ejercicio_4.py
@@ -1,5 +1,6 @@
 #Pares 1..20 (con continue) Recorre 1..20 y solo imprime los pares.
 # Usa continue para saltar los impares.
-for i in range(1,21,2):
-    
+for i in range(1,21):
+    if i % 2 :
+        continue
     print(i)
