3fa926d7e1c3bbf8dd394f11c120c556f841200d
Fabian <orleyfabian.r@gmail.com>
2025-09-14 01:24:57 -0500

    Fix logic error in total calculation and improve menu functionality in Ejercicio_11.py

--- Ejercicio_11.py
+++ Ejercicio_11.py
@@ -3,3 +3,34 @@
 # Opción 1: pide un número y lo suma al total.
 # Opción 2: muestra el total actual.
 # Opción 0: sale del menú con break. """
+
+total = 0.0
+
+
+while True:
+    print("\n  Menú básico:")
+    print("1) Sumar al total")
+    print("2) Ver total")
+    print("0) Salir")
+
+    opcion = input("Elija una opción: ")
+
+    if opcion == "1":
+        n = int(input("ingrese un valor a sumar: "))
+        total == n
+        if n == 0:
+            break
+        if n < 0:
+            continue
+        total = total + n
+        
+        
+    elif opcion == "2":
+        print(f"Total actual: {total:}")
+        
+    elif opcion == "0":
+        print("sale del menú")
+        break
+    
+    else:
+        print("Opción no válida")
