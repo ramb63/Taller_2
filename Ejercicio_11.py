9bc9a198f329c18c3476b9f7a465267db97c568f
Fabian <orleyfabian.r@gmail.com>
2025-09-14 00:23:32 -0500

    Add basic menu structure and comments for functionality in Ejercicio_11.py

---
+++ Ejercicio_11.py
@@ -0,0 +1,5 @@
+# """ Menú básico (while True + break) 
+# Menú: 1) Sumar al total 2) Ver total 0) Salir.
+# Opción 1: pide un número y lo suma al total.
+# Opción 2: muestra el total actual.
+# Opción 0: sale del menú con break. """
